8d25619576756168e1737c4f2a9f0d9d5f891b38
song <songwenjing1996@outlook.com>
2019-06-10 08:39:15 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,20 @@
 <template>
-  <div id="app">
-    index
-    <el-button type="primary">主要按钮</el-button>
+  <div>
+    <router-view></router-view>
   </div>
+    
+ 
 </template>
 
 <script>
-import axios from 'axios'
 export default {
-  // 钩子函数
-  mounted(){
-    // 获取请求
-    this.$axios({
-      url:'https://api.github.com/users',
-      // get请求参数
-      method:'GET',
-      // .then就相当于success
-    }).then(res => {
-      console.log(res); // 从url页面获取的所有参数
-      console.log(res.config.params);// 对象{abc: 123}
-    })
-  }
+
 }
 </script>
 
 <style>
-
+  * {
+    padding: 0;
+    margin: 0;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -9,7 +9,10 @@ import ElementUI from "element-ui";
 // element的样式
 import 'element-ui/lib/theme-chalk/index.css';
 
-import Login from './pages/Login.vue'
+import Login from './pages/Login.vue';
+import Admin from './pages/Admin.vue'
+import GoodsList from './pages/GoodsList.vue'
+import CategoryList from './pages/CategoryList.vue'
 Vue.prototype.$axios = axios;
 // element: 2.注册element插件
 Vue.use(ElementUI);
@@ -19,8 +22,33 @@ Vue.use(VueRouter);
 Vue.config.productionTip = false
 
 const routes = [
-  {path: "/login", component: Login},
-  {path: "/", component: Login},
+  {
+    path:'/',
+    redirect: '/admin/goods-list',
+    meta:'首页'
+  },
+  {
+    path: "/login", 
+    component: Login,
+    meta:'登陆'
+  },
+  {
+    path:'/admin',
+    component: Admin,
+    meta:'后台管理',
+    children:[
+    {
+      path:'goods-list',
+      component:GoodsList,
+      meta:'商品列表'
+    },
+    {
+      path:'category-list',
+      component:CategoryList,
+      meta:'栏目列表'
+    }
+
+  ]},
 ];
 
 // 路由实例
